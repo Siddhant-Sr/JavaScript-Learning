52287f121d304533db1a8aa67c0206a439835b87
Siddhant Srivastava <67307537+Siddhant-Sr@users.noreply.github.com>
2023-09-02 11:38:05 +0000

    conversion

---
+++ JS_basics/ConversionOperations.js
@@ -0,0 +1,19 @@
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+
+
+let isLoggedIn = "0"
+let boolisLogged = Boolean(isLoggedIn)
+console.log(boolisLogged);
