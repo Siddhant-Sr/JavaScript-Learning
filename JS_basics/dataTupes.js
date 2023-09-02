eeb0d1b538311fd8f025736ac8a978309a731a6b
Siddhant Srivastava <67307537+Siddhant-Sr@users.noreply.github.com>
2023-09-02 10:48:32 +0000

    data types

---
+++ JS_basics/dataTupes.js
@@ -0,0 +1,14 @@
+"use strict;" //treat all JS cide as newer version
+// alert(3+3) //we are using node, not browser
+
+
+console.log(3+ 3)
+console.log("sid")
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (it is typeof object)
+// undefined => the value is not defined at this point
+// symbol => unique
